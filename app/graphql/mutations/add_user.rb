86133c4adeb275a7d554a8a47bff12372ee15097
Dennis Martinez <denniswaltermartinez@gmail.com>
2023-06-17 14:10:18 -0400

    Adding comments for working test

--- app/graphql/mutations/add_user.rb
+++ app/graphql/mutations/add_user.rb
@@ -7,5 +7,10 @@ module Mutations
     def resolve(user:)
       { user: }
     end
+
+    # Should this be here?
+    # def object_from_id(_type, global_id, _query_ctx)
+    #   User.find(global_id)
+    # end
   end
 end
